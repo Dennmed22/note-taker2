3c287adbdc8c4cae0e73fb81bc8add7d5de71281
Dennmed22 <medrano.dennis@yahoo.com>
2023-08-01 16:27:04 -0400

    updated routes

--- Main/routes/htmlRoutes.js
+++ Main/routes/htmlRoutes.js
@@ -1,13 +1,11 @@
 const path = require('path');
 const router = require('express').Router();
 
-// "/notes" responds with the notes.html file
 router.get('/notes', (req, res) => {
   const notesFilePath = path.join(__dirname, '../public/notes.html');
   res.sendFile(notesFilePath);
 });
 
-// All other routes respond with the index.html file
 router.get('*', (req, res) => {
   const indexPath = path.join(__dirname, '../public/index.html');
   res.sendFile(indexPath);
